fe411dc926f087078977ad7d2914082a281d24f6
mdkawsar <info.mdkawsardev@gmail.com>
2026-02-01 22:39:31 +0600

    Some modification

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
+from .models import LoginUser
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
